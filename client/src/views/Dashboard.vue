2c946dd8f38f9dfa04e567e574eefa4bfb27cdc6
Dheeraj Gandhi <gandhidheeraj9@gmail.com>
2021-03-14 20:38:01 +0530

    added pages

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,87 @@
+<template>
+	<div class="dashboard">
+		<h1>Complains</h1>
+		<div class="list">
+			<div class="complain">
+				<div class="info">
+					<h2>Name: Dheeraj</h2>
+					<h2>Contact: 123473463</h2>
+				</div>
+				<div class="buttons">
+					<button class="view">View</button>
+					<button
+						class="delete
+                    "
+					>
+						Delete
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "Dashboard",
+};
+</script>
+<style scoped>
+.dashboard {
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	background: -webkit-linear-gradient(25rad, #000d3a, black);
+	font-family: "Poppins", sans-serif;
+
+	color: white;
+}
+h1 {
+	font-size: 1.5rem;
+	margin-bottom: 1rem;
+	margin-left: 1rem;
+}
+.list {
+	background: white;
+	border-radius: 5px;
+	margin: 1rem 1rem;
+}
+.complain {
+	padding: 0rem;
+	margin: 1rem 1rem;
+}
+
+.complain span {
+	color: #000d3ac9;
+	font-size: 1rem;
+	margin-bottom: 1rem;
+}
+button {
+	border: none;
+	margin-top: 0.4rem;
+	border-radius: 7px;
+	cursor: pointer;
+	font-size: 1rem;
+	padding: 0.5rem 2rem;
+	background: #afa1ff;
+}
+.buttons {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-gap: 2rem;
+}
+.info {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	margin-bottom: 0.2rem;
+}
+
+h2 {
+	color: #000d3ab4;
+	font-size: 1rem;
+	letter-spacing: 1px;
+}
+.delete {
+	background: rgba(255, 0, 0, 0.733);
+}
+</style>
--- client/src/views/createAccn.vue
+++ client/src/views/createAccn.vue
@@ -1,105 +1,105 @@
 <template>
-    <div class="create">
-      <h2>Create Account</h2>
-        <form class="form" @submit.prevent="create">
-            <label>
-            <span>Name:</span> <br>
-            <input type="text" required v-model="name"/>
-            </label>
-            <label>
-            <span>E-mail ID:</span> <br>
-            <input type="email" required v-model="email"/>
-            </label>
-            <label>
-            <span>Contact Number:</span> <br>
-            <input type="number" required v-modal="phno"/>
-            </label>
-            <label>
-            <span>Password:</span> <br>
-            <input type="password" required v-modal="password"/>
-            </label>
-            <label>
-            <span>Confirm Password:</span> <br>
-            <input type="password" required v-modal="password"/>
-            </label>
-            <div class="group">
-              <button class="submit" type="submit" >Create Account</button>
-              <p class="question">Already a member?</p>
-              <router-link to="/signup" class="alink">Sign Up</router-link>
-            </div>
-          </form>
-    </div>
+	<div class="create">
+		<h1>Create Account</h1>
+		<form class="form" @submit.prevent="create">
+			<label>
+				<span>Name:</span> <br />
+				<input type="text" required v-model="name" />
+			</label>
+			<label>
+				<span>E-mail ID:</span> <br />
+				<input type="email" required v-model="email" />
+			</label>
+			<label>
+				<span>Contact Number:</span> <br />
+				<input type="number" required v-modal="phno" />
+			</label>
+			<label>
+				<span>Password:</span> <br />
+				<input type="password" required v-modal="password" />
+			</label>
+			<label>
+				<span>Confirm Password:</span> <br />
+				<input type="password" required v-modal="password" />
+			</label>
+			<div class="group">
+				<button class="submit" type="submit">Create Account</button>
+				<p class="question">Already a member?</p>
+				<router-link to="/signup" class="alink">Sign in</router-link>
+			</div>
+		</form>
+	</div>
 </template>
 <script>
-
 export default {
-  name: 'LoginUser',
-  data(){
-      return{
-        name:null,
-        phno:null,
-        email:null,
-        password:null
-      }
-  },
-}
+	name: "LoginUser",
+	data() {
+		return {
+			name: null,
+			phno: null,
+			email: null,
+			password: null,
+		};
+	},
+};
 </script>
 <style scoped>
-.create{
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column ;
-  background: -webkit-linear-gradient(25rad, #000D3A, black);
-  font-family:'Roboto', sans-serif;
-  text-align: center;
-  color: white;
+.create {
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	background: -webkit-linear-gradient(25rad, #000d3a, black);
+	font-family: "Roboto", sans-serif;
+	text-align: center;
+	color: white;
 }
-h2{
-  font-size: 1.3rem;
-  font-weight: 500;
-  position: relative;
-  bottom: 1rem;
+h1 {
+	margin-bottom: 3rem;
 }
-.form{
-  text-align: start;
-  height: 70vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+.form {
+	text-align: start;
+	height: 70vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 }
-.group{
-  text-align: center;
+.group {
+	text-align: center;
 }
-label span, .submit{
-  font-size: 0.9rem;
-  font-weight: 500;
+span {
+	font-size: 1.2rem;
 }
-label input{
-  margin-top: 0.5rem;
-  border-radius: 7px;
-  border: none;
-  outline: none;
-  height: 1.3rem;
-  width: 14rem;
+label input {
+	margin-top: 0.5rem;
+	border-radius: 7px;
+	border: none;
+	padding: 1rem;
+	outline: none;
+	height: 1.3rem;
+	width: 14rem;
 }
-.submit{
-  margin-top: 1rem;
-  border: none;
-  border-radius: 7px;
-  cursor: pointer;
-  width: 7.6rem;
-  padding: 0.3rem ;
-  background: #AFA1FF;
-  font-weight: 500;
+.submit {
+	margin-top: 2rem;
+	border: none;
+	border-radius: 7px;
+	cursor: pointer;
+	font-size: 1rem;
+	padding: 0.7rem 3rem;
+	background: #afa1ff;
+	margin-bottom: 1rem;
 }
-.alink{
-  color: white;
-  text-decoration: none;
-  font-family: 'Poppins';
-  padding: 0.3rem 0;
-  border-bottom: 1px solid white;
+.alink {
+	color: white;
+	text-decoration: none;
+	font-family: "Poppins";
+	padding-bottom: 0.3rem;
+	border-bottom: 1px solid white;
+}
+.question {
+	margin-top: 1rem;
+	margin-bottom: 0.5rem;
 }
 </style>
